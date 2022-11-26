db4b44ad087f9e0d044dddce54fca9a1f72b2440
Darrel Valdivieso <valdivieso4@hotmail.com>
2022-11-25 23:42:05 -0500

    Progress on filling in game.js and phrase.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+
+    constructor() {
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    startGame(){
+        this.activePharse  = getRandomPhrase();
+        addPhraseToDisplay(activePhrase);
+    }
+
+    getRandomPhrase(){
+
+        //randomly calls on
+        const len = this.phrases.length;
+        const randNum = Math.floor(Math.random()*len);
+        return this.phrases[randNum];
+    }
+
+    addPhraseToDisplay(){
+
+    }
+
+    handleInteraction(){
+
+
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+
+
+let template = (listing)=
+    `<ul>
+    ${listing}
+        </ul>`
+
+class Phrase {
+
+    constructor(pharse) {
+        this.phrase = pharse.toLowerCase();
+    }
+
+
+    addPhraseToDisplay(pharse){
+    const phraseLength = pharse.length;
+
+    pharse.forEach()
+
+    }
+
+
+    checkLetter() {
+
+    }
+
+    showMatchedLetter(){
+
+    }
+}
